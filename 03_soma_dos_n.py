e7d711adf6bd957753a7c224ceddb263abd78c95
Milton Bezerra Filho <miltonbfilho206@gmail.com>
2025-07-28 11:30:09 -0300

    Update 03_soma_dos_n.py

--- 03_soma_dos_n.py
+++ 03_soma_dos_n.py
@@ -1,10 +1,13 @@
 # Faça um programa que leia um valor inteiro positivo n e determina a soma dos n primeiros números pares positivos (utilize o comando for).  Ex.: Para 𝑛 = 3, 𝑠𝑜𝑚𝑎 = 2 + 4 + 6. 
 
-entrada = int( input("Digite um número: "))
+entrada = int( input("Digite um número inteiro e postivo: "))
 soma = 0
 par = 2
 
-for n in range(0, entrada):
-    soma = soma + par
-    par += 2
+if (entrada <= 0):
+    print("Número inválido.")
+else:
+    for n in range(0, entrada):
+        soma = soma + par
+        par += 2
 print(f'A soma é {soma}')
